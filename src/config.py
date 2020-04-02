c50d1a044bd357990bc9d10d7c5dba4e3a8fb362
Hybin Hwang <hhb8550@live.com>
2020-04-02 14:18:35 +0800

    initial commit

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import json
+
+
+class Config(object):
+    conf_file = "../config/config.json"
+
+    def __init__(self):
+        with open(self.conf_file, encoding="utf-8") as cf:
+            conf_dict = json.load(cf)
+
+        for key, value in conf_dict.items():
+            setattr(self, key, value)
